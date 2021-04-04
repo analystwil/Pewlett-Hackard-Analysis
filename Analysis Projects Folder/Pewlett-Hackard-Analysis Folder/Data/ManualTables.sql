79e22bdab3d7caa26fd33bc90a6d1b25d73287d3
analystwil <ashley.gibson12@gmail.com>
2021-04-04 11:09:34 -0400

    commiting queries

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/ManualTables.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/ManualTables.sql
@@ -125,3 +125,125 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM Retirement_Info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM "Employees"
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM "Departments" as d
+INNER JOIN "Managers" as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    "Dept_Emp".to_date
+FROM retirement_info
+LEFT JOIN "Dept_Emp"
+ON retirement_info.emp_no = "Dept_Emp".emp_no;
+
+-- Joining retirement_info and dept_emp tables w/ aliases 
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN "Dept_Emp" as de
+ON ri.emp_no = de.emp_no;
+
+-- retirement-eligible employees
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN "Dept_Emp" as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO empcount_dept
+FROM current_emp as ce
+LEFT JOIN "Dept_Emp" as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM "Salaries"
+ORDER BY to_date DESC;
+
+-- select columns from three tables, create a new temp table, add aliases, join two of the three tables 
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+--INTO emp_info
+FROM "Employees" as e
+INNER JOIN "Salaries" as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN "Dept_Emp" as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	      AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM "Managers" AS dm
+    INNER JOIN "Departments" AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+-- department retirees 
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN "Dept_Emp" AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN "Departments" AS d
+ON (de.dept_no = d.dept_no);
+
+--Create a query that will return only the information relevant to the Sales team
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+	d.dept_name
+INTO sales_team
+FROM "Employees" as e
+INNER JOIN "Dept_Emp" AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN "Departments" AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT * FROM sales_team
+WHERE dept_name IN ('Sales');
+
+-- Check the table
